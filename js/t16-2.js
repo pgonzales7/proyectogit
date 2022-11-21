d3b9ab644c1f499c73e7037252a8688764c712e9
pgonzales7 <118759321+pgonzales7@users.noreply.github.com>
2022-11-21 17:08:28 -0500

    Add files via upload

---
+++ js/t16-2.js
@@ -0,0 +1,10 @@
+// En base al siguiente código: añadir una función para pasar de Farenheit a Celsius: 
+
+let pasaraFarenheit = (grado) => {return Fahrenheit = grado * 1.8 + 32;};
+
+var result = pasaraFarenheit(22);
+alert(result);
+
+let pasarCelsius = (grado) => {return Celsius = (grado - 32)*(5/9);};
+var result = pasarCelsius(50);
+alert(result);
