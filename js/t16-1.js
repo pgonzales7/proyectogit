ed1d0e20e71bcbdd8919f6f320f9c7f738a10fdf
pgonzales7 <118759321+pgonzales7@users.noreply.github.com>
2022-11-21 17:08:09 -0500

    Add files via upload

---
+++ js/t16-1.js
@@ -0,0 +1,20 @@
+let multipli = function(x,y){
+    return x*y
+}
+
+let expon = function (x,y){
+    return Math.pow(x, y);
+}
+
+let suma = (x,y) => {return x+y};
+
+let restar = (x,y) => {return x-y};
+
+var result = multipli(5,2);
+alert (result);
+
+result = suma (3,4);
+alert (result);
+
+result = restar (8,3);
+alert(result);
